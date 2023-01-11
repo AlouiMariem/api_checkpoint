833e423a8831c94390c08957e44dd1912a8379e5
AlouiMariem <[200~ --global user.name Your>
2023-01-11 14:30:06 +0100

    api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Routes,Route}from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import UserList from './Components/UserList';
+import Details from './Components/Details'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <h4 className='note'> CLICK  FOR MORE DETAILS </h4>
+      <Routes>
+        <Route path='/' element={< UserList />} />
+        <Route path='Details/:id' element={<Details />} />      
+      </Routes>
+      <br/>
+
     </div>
   );
 }
